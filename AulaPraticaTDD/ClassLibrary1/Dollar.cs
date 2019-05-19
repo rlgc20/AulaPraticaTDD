a88e8f860c60e701429e7f2e7ce24013c2fbca9f
Ronaldo <ronaldolucas_gc@hotmail.com>
2019-05-19 16:34:23 -0300

    Passo 1 - classes

---
+++ AulaPraticaTDD/ClassLibrary1/Dollar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Dollar
+    {
+        public int amount = 50;
+        
+        public Dollar(int amount)
+        {
+
+        }
+
+        public void times(int multiplier) { }
+    }
+}
---
+++ AulaPraticaTDD/ClassLibrary1/DollarTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    [TestClass()]
+    public class DollarTest
+    {
+        [TestMethod()]
+        public void testMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.times(2);
+            Assert.AreEqual(10, five.amount);
+        }
+    }
+}
